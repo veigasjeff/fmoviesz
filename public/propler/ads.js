cac85e44f8eebd69b1c967a4f53a855524924c41
mxplayermovies <110382877+veigasjeff@users.noreply.github.com>
2024-09-11 11:12:47 +0530

    ADS UPDATED

--- public/propler/ads.js
+++ public/propler/ads.js
@@ -1,41 +1,8 @@
-      // // Function to load the ad code
-      // function loadAdCode() {
-      //   var adScript = document.createElement('script');
-      //   adScript.async = true;
-      //   adScript.setAttribute('data-cfasync', 'false');
-      //   adScript.src = '//thubanoa.com/1?z=7762649';
-      //   document.body.appendChild(adScript);
-      // }
-
-      // // Set a timeout to load the ad code after 5 seconds
-      // setTimeout(loadAdCode, 8000); // 5000 milliseconds = 5 seconds
-
-
-      // // <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7762649"></script>
-
-      // Function to load the ad code
-// function loadAdCode() {
-//   var adScript = document.createElement('script');
-//   adScript.src = 'https://alwingulla.com/88/tag.min.js';
-//   adScript.setAttribute('data-zone', '92271');
-//   adScript.async = true;
-//   adScript.setAttribute('data-cfasync', 'false');
-
-//   // Handle script load errors
-//   adScript.onerror = function() {
-//     console.error('Failed to load ad script');
-//   };
-
-//   document.body.appendChild(adScript);
-// }
-
-// // Set a timeout to load the ad code after 15 seconds
-// setTimeout(loadAdCode, 10000); // 15000 milliseconds = 15 seconds
 
 // Function to load the ad code
 function loadAdCode() {
   var adScript = document.createElement('script');
-  adScript.src = '//thubanoa.com/1?z=7989315';
+  adScript.src = '//thubanoa.com/1?z=8074720';
   adScript.async = true;
   adScript.setAttribute('data-cfasync', 'false');
 
@@ -51,4 +18,4 @@ function loadAdCode() {
 // Set a timeout to load the ad code after 10 seconds
 setTimeout(loadAdCode, 5000); // 10000 milliseconds = 10 seconds
 
-{/* <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7989315"></script> */}
+{/* <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=8074720"></script> */}
