4386349bcf0eb6c36592bb23171bc330a72a9795
mxplayermovies <110382877+veigasjeff@users.noreply.github.com>
2024-09-14 10:48:11 +0530

    clear now

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://fmoviesz.vercel.app/',
-  changefreq: 'daily',
-  priority: 0.9, // Set priority to just below the highest value
-  // generateRobotsTxt: true,
-  sitemapSize: 7000,
-};
