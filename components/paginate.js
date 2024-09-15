bc5ec5eafe9a3818a36039f1f884760588ec6400
mxplayermovies <110382877+veigasjeff@users.noreply.github.com>
2024-09-15 20:24:47 +0530

    clean all

--- components/paginate.js
+++
@@ -1,7 +0,0 @@
-// utils/paginate.js
-export function paginate(items, currentPage, itemsPerPage) {
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    return items.slice(startIndex, endIndex);
-  }
-  
